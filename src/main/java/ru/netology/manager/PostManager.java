dbfc8a29ff135d6336ee41bf4117a4ca14d42ac4
nikola4468 <nikola4468@gmail.com>
2021-09-13 17:41:50 +0900

    add PostManager

--- src/main/java/ru/netology/manager/PostManager.java
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -2,7 +2,13 @@ package ru.netology.manager;
 
 import ru.netology.domain.PostVk;
 
-public class PostManager {
-    private PostVk post;
+public class PostManager{
+    private PostVk[] post;
+
+    public PostVk[] search(int ownerId, String domain, String query, int ownersOnly, int count, int offset) {
+        return null;
+    }
+    public void delete(int ownerId, int postId) {
+    }
 
 }
